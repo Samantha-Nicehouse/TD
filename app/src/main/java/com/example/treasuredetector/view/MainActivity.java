107bccecd845b38e56f833a0f9d5df84cb371165
Samantha Nettesheim <snetteshe@gmail.com>
2021-04-01 16:03:39 +0200

    fragments Added
    added Fragment xmls but not fragment code

---
+++ app/src/main/java/com/example/treasuredetector/view/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.treasuredetector.view;
+
+/*Activity is nothing but a java class in Android which has some
+ pre-defined functions which are triggered
+at different App states, which we can override to perform anything we want.*/
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.treasuredetector.R;
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+/*apart from controlling the app, Activity also controls creation,
+destruction and other states of the App's lifecycle.*/
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, new HomeFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    if (item.getItemId() == R.id.navBar_home) {
+                        selectedFragment = new HomeFragment();
+                    } else if (item.getItemId() == R.id.navBar_profile) {
+                        selectedFragment = new ProfileFragment();
+                    } else if (item.getItemId() == R.id.navBar_map) {
+                        selectedFragment = new MapFragment();
+                    } else if (item.getItemId() == R.id.navBar_lists) {
+                        selectedFragment = new ListFragment();
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, selectedFragment).commit();
+                    return true;
+                }
+            };
+}
